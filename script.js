b5f0c686a0e6467a6fac3ed01756a7c74feefb1b
IsaRdz <isabelrodriguez.dg@gmail.com>
2022-12-19 11:26:07 -0300

    :bug: fix display of input

--- script.js
+++ script.js
@@ -2,22 +2,33 @@ const resultado = document.getElementById('resultado');
 const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
 
+var op1;
+var op2;
+//!isNaN(event.key) &&
 window.addEventListener("keydown",(event)=> {
   console.log("event",event.key);
-  if(!isNaN(event.key) && resultado.textContent.length < 13){
-    resultado.textContent = resultado.textContent + event.key;
+  if(resultado.textContent.length < 13){
+    if(isNaN(event.key) || resultado.textContent == 0){
+      resultado.textContent = event.key;
+      console.log("resultado.textContent",resultado.textContent);
+    }else {
+      resultado.textContent = resultado.textContent + event.key;
+    }
   }
 });
 
 for(const number of numbers){
   number.addEventListener("click", (event) => {
     console.log("numbers",number.innerHTML);
-
     if(resultado.textContent.length < 13){
-      resultado.textContent = resultado.textContent + number.innerHTML;
+      if(resultado.textContent == 0 || resultado.textContent == "/" || resultado.textContent == "x" || resultado.textContent == "-" || resultado.textContent == "+" || resultado.textContent == "%"){
+        resultado.textContent = number.innerHTML;
+      }else{
+        resultado.textContent = resultado.textContent + number.innerHTML;
+      }
     }
-  })
-}
+  });
+};
 
 for(const operator of operators){
   operator.addEventListener( "click", ()=> {
